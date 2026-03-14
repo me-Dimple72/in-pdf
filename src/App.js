b9ea739557f3bd40ed2eef2a09bfc5465626840a
Dimple Goyal <dimpleagarwal155@gmail.com>
2026-03-14 18:04:04 +0530

    connect frontend to render backend

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import * as pdfjsLib from "pdfjs-dist";
 
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
-const BACKEND = "http://localhost:5000";
+const BACKEND = "https://in-pdf-api.onrender.com";
 
 export default function App() {
  
